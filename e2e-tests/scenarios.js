b1484f22974ba845f5df2bd65dcee6c5c95fe233
Jerome Pratt <jerome.pratt@outlook.com>
2014-12-06 20:43:53 +0000

    refactoring of e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -9,14 +9,14 @@ describe('shopApp', function() {
       var secondItem = element(by.repeater('product in products').row(1))
       var sharkSkin = element(by.repeater('product in products').row(9))
       var outOfStockFlipFlops = element(by.repeater('product in products').row(4))
-      var addFirstItem = firstItem.element(by.css('#add-item'))
-      var addSecondItem = secondItem.element(by.css('#add-item'))
-      var addSharkSkin = sharkSkin.element(by.css('#add-item'))
-      var addFlipFlops = outOfStockFlipFlops.element(by.css('#add-item'))
       var firstCartItem = element(by.repeater('item in invoice').row(0))
       var rmFirstCartItem = firstCartItem.element(by.css('#remove-item'))
       var subTotal = element(by.css('#sub-total'))
       var finalCost = element(by.css('#final-cost'))
+
+      var addItem = function(item){
+          item.element(by.css('#add-item')).click()
+      }
 
     beforeEach(function() {
       browser.get('/app');
@@ -28,42 +28,41 @@ describe('shopApp', function() {
   });
 
   it('should add an item to shopping cart when requested', function(){
-      var shoppingCart = element.all(by.repeater('item in invoice'));
       expect(shoppingCart.count()).toBe(0)
-      addFirstItem.click()
+      addItem(firstItem)
       expect(shoppingCart.count()).toBe(1)
   })
 
 
   it('should remove an item from shopping cart when requested', function(){
-      addFirstItem.click()
+      addItem(firstItem)
       expect(shoppingCart.count()).toBe(1)
       rmFirstCartItem.click()
       expect(shoppingCart.count()).toBe(0)
   })
 
   it('should be able to view the total price when there is a product in cart', function(){
-      addFirstItem.click()
+      addItem(firstItem)
       expect(subTotal.getText()).toBe('£99.00')
       expect(finalCost.getText()).toBe('£99.00')
   })
 
   it('should be able to view the total price of multiple products in cart', function(){
-      addFirstItem.click()
-      addSecondItem.click()
+      addItem(firstItem)
+      addItem(secondItem)
       expect(subTotal.getText()).toBe('£141.00')
       expect(finalCost.getText()).toBe('£141.00')
   })
 
   it('should be able to apply a voucher to shopping cart and see a reduced price.', function(){
-      addFirstItem.click()
+      addItem(firstItem)
       element(by.css('#standard-discount')).click()
       expect(subTotal.getText()).toBe('£99.00')
       expect(finalCost.getText()).toBe('£94.00')
   })
 
   it('should not be able to apply a £10 voucher when less than £50 is being spent', function(){
-      addSecondItem.click()
+      addItem(secondItem)
       element(by.css('#over-fifty')).click()
       expect(subTotal.getText()).toBe('£42.00')
       expect(finalCost.getText()).toBe('£42.00')
@@ -71,7 +70,7 @@ describe('shopApp', function() {
   })
 
   it('should be able to apply a £10 voucher when more than £50 is being spent', function(){
-      addSharkSkin.click()
+      addItem(sharkSkin)
       element(by.css('#over-fifty')).click()
       expect(element(by.css('.alert-msg')).isPresent()).toBe(false)
       expect(subTotal.getText()).toBe('£75.00')
@@ -79,7 +78,7 @@ describe('shopApp', function() {
   })
 
   it('should not be able to apply a £15 voucher when less than £75 is being spent', function(){
-      addSecondItem.click()
+      addItem(secondItem)
       element(by.css('#over-seventyFive')).click()
       expect(element(by.css('.alert-msg')).getText()).toBe('You must purchase some footwear and spend over £75 in order to use this discount.')
       expect(subTotal.getText()).toBe('£42.00')
@@ -87,7 +86,7 @@ describe('shopApp', function() {
   })
 
   it('should not be able to apply a £15 voucher when footwear is not being bought', function(){
-      addSharkSkin.click()
+      addItem(sharkSkin)
       element(by.css('#over-seventyFive')).click()
       expect(element(by.css('.alert-msg')).getText()).toBe('You must purchase some footwear and spend over £75 in order to use this discount.')
       expect(subTotal.getText()).toBe('£75.00')
@@ -95,7 +94,7 @@ describe('shopApp', function() {
   })
 
   it('should be able to apply a £15 voucher when footwear is being bought and £75 is being spent', function(){
-      addFirstItem.click()
+      addItem(firstItem)
       element(by.css('#over-seventyFive')).click()
       expect(element(by.css('.alert-msg')).isPresent()).toBe(false)
       expect(subTotal.getText()).toBe('£99.00')
@@ -103,7 +102,7 @@ describe('shopApp', function() {
   })
 
   it('should not be able to add out of stock items to shopping cart', function(){
-      addFlipFlops.click()
+      addItem(outOfStockFlipFlops)
       expect(element(by.css('.alert-msg')).getText()).toBe('Sorry, this item is out of stock')
       expect(subTotal.getText()).toBe('')
   })
